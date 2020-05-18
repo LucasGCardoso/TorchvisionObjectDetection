4b635ae56dd2c4b4b0b0a72bb1aca97d116439a2
Lucas Cardoso <40406397+LucasGCardoso@users.noreply.github.com>
2020-05-18 16:21:27 -0300

    Adding the root files
    Adding the root files

---
+++ image-test.py
@@ -0,0 +1,13 @@
+from PIL import Image
+Image.open('PennFudanPed/PNGImages/FudanPed00001.png')
+mask = Image.open('PennFudanPed/PedMasks/FudanPed00001_mask.png')
+# each mask instance has a different color, from zero to N, where
+# N is the number of instances. In order to make visualization easier,
+# let's adda color palette to the mask.
+mask.putpalette([
+    0, 0, 0, # black background
+    255, 0, 0, # index 1 is red
+    255, 255, 0, # index 2 is yellow
+    255, 153, 0, # index 3 is orange
+])
+mask
